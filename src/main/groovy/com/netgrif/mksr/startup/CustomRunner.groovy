84994c1e1c15cf2a1d9b9bcb97d53ee649f6081d
martinkranec <kranec@netgrif.com>
2024-04-12 12:59:10 +0200

    - create user - added subject proces

--- src/main/groovy/com/netgrif/mksr/startup/CustomRunner.groovy
+++ src/main/groovy/com/netgrif/mksr/startup/CustomRunner.groovy
@@ -5,6 +5,7 @@ import com.netgrif.application.engine.auth.service.interfaces.IUserService
 import com.netgrif.application.engine.petrinet.service.interfaces.IPetriNetService
 import com.netgrif.application.engine.startup.AbstractOrderedCommandLineRunner
 import com.netgrif.application.engine.workflow.service.interfaces.IWorkflowService
+import com.netgrif.mksr.CustomActionDelegate
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -24,8 +25,19 @@ class CustomRunner extends AbstractOrderedCommandLineRunner {
     @Autowired
     private IPetriNetService petriNetService
 
+    @Autowired
+    private CustomActionDelegate actionDelegate
+
     @Override
     void run(String... args) throws Exception {
         log.info("Calling EtaskRunner runner")
+        try {
+            actionDelegate.createNewUser("Jožko", "Daxner", "daxner@netgrif.com", "password")
+            actionDelegate.createNewUser("Jožko", "Macháč", "machac@netgrif.com", "password")
+            actionDelegate.createNewUser("Maťko", "Kranec", "kranec@netgrif.com", "password")
+            actionDelegate.createNewUser("Ľubko", "Petrovič", "petrovic@netgrif.com", "password")
+        } catch (Exception e) {
+            log.warn("Users already created", e)
+        }
     }
 }
